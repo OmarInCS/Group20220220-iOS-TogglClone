64a3eb28bf3e7422a715fa8b40b625ec4e3464ee
OmarInCS <Omar.Abdelkarim@tuwaiq.edu.sa>
2022-02-23 10:49:25 +0300

    picker view

--- TogglClone/BottomSheetVC.swift
+++ TogglClone/BottomSheetVC.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class BottomSheetVC: UIViewController {
-
+    
+    
+    @IBOutlet weak var lbDuration: UILabel!
+    
+    @IBOutlet weak var tfDescription: UITextField!
+    
+    @IBOutlet weak var tfProject: UITextField!
+    
+    @IBOutlet weak var dpStartTime: UIDatePicker!
+    
+    @IBOutlet weak var dpEndTime: UIDatePicker!
+    
+    var pvProjects = UIPickerView()
+    var selectedProject: Project!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        pvProjects.dataSource = self
+        pvProjects.delegate = self
+        tfProject.inputView = pvProjects
     }
+    
+    @IBAction func closeBottomSheet(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func addTimeEntry(_ sender: Any) {
+        
+        
+        if tfDescription.text == nil || tfDescription.text == "" {
+            print("discription is missing")
+            return
+        }
+        
+        let newEntry = TimeEntry(startTime: dpStartTime.date, description: tfDescription.text!, endTime: dpEndTime.date, project: selectedProject)
+        TimeEntry.dummyEntries.append(newEntry)
+        self.dismiss(animated: true, completion: nil)
+
+    }
+    
     
 
     /*
@@ -27,3 +65,30 @@ class BottomSheetVC: UIViewController {
     */
 
 }
+
+
+extension BottomSheetVC: UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return Project.dummyProjects.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return Project.dummyProjects[row].projectName
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        selectedProject = Project.dummyProjects[row]
+        tfProject.text = selectedProject.projectName
+        tfProject.resignFirstResponder()
+    }
+    
+}
+
+
